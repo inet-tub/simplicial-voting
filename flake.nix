11fd08e368c6ebf939d07308ad5554b6e934d8a6
berber <git@zmberber.com>
2023-10-25 22:33:56 +0200

    no flake-utils, this hangs!

--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
-  description = "Python environment for simplicial voting project";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv;
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
       {
-        devShells.default =
-          let
-            simplicial-voting = poetry2nix.legacyPackages.${system}.mkPoetryEnv {
-              projectDir = ./.;
-            };
-          in simplicial-voting.env;
-      });
+        # packages = forAllSystems (system: {
+        #   default = poetry2nix.legacyPackages.${system}.mkPoetryApplication { projectDir = self; };
+        # });
+
+        devShells = forAllSystems (system: {
+          default = pkgs.${system}.mkShellNoCC {
+            packages = [
+              (poetry2nix.legacyPackages.${system}.mkPoetryEnv { projectDir = self; })
+              poetry2nix.poetry
+            ];
+          };
+        });
+      };
 }
